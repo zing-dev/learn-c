20d6888d5d0586d656e086ad947e3985af3be2fb
zhangrongxiang <zhangrxiang@yahoo.com>
2017-08-08 23:04:47 +0800

    bsearch() sizeof()

---
+++ 2017/08/08/1.c
@@ -0,0 +1,13 @@
+//
+// Created by 张荣响 on 2017/8/8.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int x = 5;
+    printf("%d and ", sizeof(x++)); // note 1
+    printf("%d\n", x); // note 2
+    return 0;
+}
