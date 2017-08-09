23c5ca863a5a2f2e61811781d3215339476ad4e3
zhangrongxiang <zhangrxiang@yahoo.com>
2017-08-09 18:03:25 +0800

    bzero() memset()

---
+++ string/2.c
@@ -0,0 +1,24 @@
+//
+// Created by 张荣响 on 2017/8/9.
+//
+
+
+#include <stdio.h>
+#include <strings.h>
+
+
+//置字节字符串前n个字节为零且包括‘\0’。
+
+int main(){
+    char str[10] = {'a','b','c','d'};
+
+    printf("%s\n",str);
+    bzero(&str,4);
+    printf("%s\n",str);
+
+    memset(&str,'*', sizeof(str)-1);
+    printf("%s\n",str);
+    bzero(&str, sizeof(str));
+    printf("%s\n",str);
+    printf("------------------------\n");
+}
