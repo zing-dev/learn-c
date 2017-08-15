8ef19700822c80e26e8343bf0bb1f6c37d628d70
zhangrongxiang <zhangrxiang@debian>
2017-08-15 16:34:35 +0800

    *((unsigned long *)buflist) = *((unsigned long *)ptr);

---
+++ 2017/08/15/pointer.c
@@ -0,0 +1,40 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+
+int main()
+{
+
+	char *ptr = NULL;
+	char *buflist = NULL;
+
+	printf("buflist length is %d\n",sizeof(buflist));
+	printf("char* length is %d\n",sizeof(char *));
+	printf("length is %d\n",sizeof(buflist)/sizeof(buflist[0]));
+
+
+	if((buflist = malloc(1024)) == NULL){
+		printf("malloc error .... ...");
+		exit(-1);
+	}
+
+	printf("buflist length is %d\n",sizeof(buflist));
+	printf("char length is %d\n",sizeof(char));
+
+
+	*((unsigned long*)buflist) = 0;
+	printf("buflist[0] is %d\n",buflist[0]);
+	printf("buflist[1] is %d\n",buflist[1]);
+	printf("buflist[2] is %d\n",buflist[2]);
+
+
+
+	printf("buflist length is %d\n",sizeof(buflist));
+
+	ptr =(char *)(*((unsigned long *)buflist));
+	printf("ptr--->%d\n",sizeof(ptr));
+	*((unsigned long *)buflist) = *((unsigned long *)ptr);
+
+	printf("%d\n",buflist[0]);
+
+}
