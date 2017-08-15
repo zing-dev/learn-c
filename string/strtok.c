518f960d8f253ee378a179e9822c2c4d86e44f86
张荣响 <zhangrxiang@yahoo.com>
2017-08-15 12:52:00 +0800

    atoi() strtok()

---
+++ string/strtok.c
@@ -0,0 +1,59 @@
+//
+// Created by zhangrongxiang on 2017/8/15 10:59.
+// Copyright (c) 2017 ZRC . All rights reserved.
+//
+
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+void strtoktest()
+{
+    char input[16] = "abc=d";
+    char *p;
+    /*strtok places a NULL terminator
+    infront of the token,if found*/
+    p = strtok(input, "=");
+    if (p)
+        printf("%s\n", p);
+    /*Asecond call to strtok using a NULL
+    as the first parameter returns a pointer
+    to the character following the token*/
+    p = strtok(NULL, "=");
+    if (p)
+        printf("%s\n", (p));
+}
+int strtoktest2()
+{
+    char input[27] = "TPROXY_CPORT = 11111";
+    char *temp;
+    if ((temp = (char *) strtok (input, " "))){
+        printf("-->  %s\n",temp);
+    }
+    if ((temp = (char *) strtok (NULL, " "))){
+        printf("---->  %s\n",(temp));
+    }
+//    if ((temp = (char *) strtok (NULL, " "))){
+//        printf("---->  %s\n",temp);
+//    }
+
+
+
+    if(!strcmp(temp, "TPROXY_CPORT"))
+    {
+        printf("-------> %d\n",temp);
+        if((temp = (char *) strtok (NULL, " ")) == NULL)
+            return 1;
+        int GF_TPROXY_CPORT = atoi(temp);
+        printf("--> %d\n",temp);
+        printf("%d\n",GF_TPROXY_CPORT);
+        printf("---------------------end---------------------");
+    }
+    return 1;
+}
+int main(void)
+{
+    strtoktest();
+//    strtoktest2();
+    return 0;
+}
