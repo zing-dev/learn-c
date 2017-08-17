4f0eb8ae13c13ff63c9efecffa0ffc1fd824a3f0
张荣响 <zhangrxiang@yahoo.com>
2017-08-17 15:08:47 +0800

    str + 1, &str + 1 取值比较

---
+++ pointer/3.c
@@ -0,0 +1,15 @@
+//
+// Created by zhangrongxiang on 2017/8/17 12:09.
+// Copyright (c) 2017 ZRX . All rights reserved.
+//
+
+#include <stdio.h>
+int main(void)
+{
+    char str[] = "zhangrongxiang";
+    char *p = str;
+    printf("str   = %p        p        = %p\n", str, p);
+    printf("str+1 = %p        &str + 1 = %p\n", str + 1, &str + 1);
+    printf("p+1   = %p\n", p + 1);
+    return 0;
+}
