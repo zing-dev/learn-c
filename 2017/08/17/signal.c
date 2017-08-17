068d2a65b3f26ada99e4dc9c1da4b8d753599f09
zhangrxiang <zhangrxiang@yahoo.com>
2017-08-17 22:05:03 +0800

    signal

---
+++ 2017/08/17/signal.c
@@ -0,0 +1,23 @@
+//
+// Created by zhangrxiang on 17-8-17.
+//
+
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void ouch(int sig) {
+    printf("I got signal %d\n", sig);
+    // (void) signal(SIGINT, SIG_DFL);
+    //(void) signal(SIGINT, ouch);
+
+}
+
+int main() {
+    (void) signal(SIGINT, ouch);
+
+    while (1) {
+        printf("hello world...\n");
+        sleep(1);
+    }
+}
