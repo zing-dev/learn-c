7704dc5079d372944c997c26bd3c81b408fc5b11
zhangrongxiang <zhangrxiang@debian>
2017-08-29 16:47:11 +0800

    popen()

---
+++ 2017/08/29/popen.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main()
+{
+	FILE* fp = NULL;
+	char cmd[512];
+//	sprintf(cmd, "ls 2>/dev/null; echo $?");
+	sprintf(cmd, "ls /");
+	printf("%s\n",cmd);
+    if ((fp = popen(cmd, "r")) != NULL){
+
+//		while((fread(cmd,sizeof(cmd),sizeof(cmd),fp)) != '\0'){
+//			puts(cmd);
+//			printf("-->  %s\n",cmd);
+//		}
+
+		memset(cmd,0,sizeof(cmd));
+		fgets(cmd, sizeof(fp), fp);
+		printf("%s\n",cmd);
+		pclose(fp);
+	}//0 成功， 1 失败
+	printf("cmd is %s\n", cmd);
+	return 0;
+}
