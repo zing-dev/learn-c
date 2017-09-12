fcb603abe0ce3cd722948282223660c43ea2465b
zhangrxiang <599490911@qq.com>
2017-09-12 15:40:23 +0800

    udp1.0

---
+++ 2017/09/12/udp/client.c
@@ -0,0 +1,49 @@
+//
+// Created by zhangrongxiang on 2017/9/12.
+//
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#define MAXBUF 256
+int main()
+{
+    struct sockaddr_in server;
+    char *addr = "127.0.0.1";
+    int port = 1995;
+    int socketfd = 0;
+    char buf[MAXBUF] = {0};
+
+    bzero(&server,sizeof(server));
+
+    server.sin_family = PF_INET;
+    server.sin_addr.s_addr = inet_addr(addr);
+    server.sin_port = htons(port);
+
+    socketfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(socketfd < 0){
+        printf("create socket error ... ");
+        return -1;
+    }
+
+    while (1){
+        memset(buf, 0, MAXBUF);
+        fgets(buf, MAXBUF, stdin);
+        int send = sendto(socketfd, buf, strlen(buf), 0,
+               (struct sockaddr *) &server,
+               sizeof(struct sockaddr)
+        );
+
+        if(send < 0){
+            perror("sendto error ....");
+            break;
+        } else{
+            printf("send to %s(port=%d) len %d:%s\n",
+                   addr, port, send, buf);
+        }
+    }
+
+}
---
+++ 2017/09/12/udp/server.c
@@ -0,0 +1,58 @@
+//
+// Created by zhangrongxiang on 2017/9/12.
+//
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#define MAXBUF 256
+
+int main(int argc, char *argv[]) {
+    int port = 1995;
+    int s = 0;
+    int reuse = 0;
+    int recv = 0;
+    char buf[MAXBUF] = {0};
+    struct sockaddr_in server, client;
+    int sock_len = sizeof(struct sockaddr);
+
+    bzero(&server, sizeof(server));
+    server.sin_family = PF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(port);
+
+    s = socket(AF_INET, SOCK_DGRAM, 0);
+    if (s < 0) {
+        perror("create socket error ... ");
+        return -1;
+    }
+
+    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+
+    if (bind(s, (struct sockaddr *) &server, sizeof(server)) < 0) {
+        perror("bind error ...");
+        return -1;
+    }
+    printf("server recvfrom........................................\n");
+    while (1) {
+        memset(buf, 0, MAXBUF);
+        recv = recvfrom(
+                s,
+                buf,
+                MAXBUF,
+                0,
+                (struct sockaddr *) &client,
+                &sock_len
+        );
+
+        if(recv < 0){
+            perror("recvfrom error ... \n");
+            break;
+        } else{
+            printf("receive msg from %s(port=%d) len %d: %s\n",inet_ntoa(client.sin_addr), port, recv, buf);
+        }
+    }
+    return 0;
+}
