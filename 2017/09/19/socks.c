7483fab037168f7b4e5576785112bc83d3a23d68
zhangrxiang <599490911@qq.com>
2017-09-20 15:14:23 +0800

    socks

---
+++ 2017/09/19/socks.c
@@ -0,0 +1,56 @@
+//
+// Created by zhangrongxiang on 2017/9/19 14:12
+//
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define SERVER_PORT 8080
+
+int main(int argc, char *argv[]) {
+    int clientSocket;
+    struct sockaddr_in serverAddr;
+    char sendbuf[200];
+    char recvbuf[200];
+    int iDataNum;
+    if ((clientSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(SERVER_PORT);
+    serverAddr.sin_addr.s_addr = inet_addr("192.168.33.10");
+    if (connect(clientSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0) {
+        perror("connect");
+        return 1;
+    }
+
+    printf("connect with destination host...\n");
+
+    while (1) {
+        printf("Input your world:>");
+        scanf("%s", sendbuf);
+        printf("\n");
+
+        send(clientSocket, sendbuf, strlen(sendbuf), 0);
+        if (strcmp(sendbuf, "quit") == 0)
+            break;
+        iDataNum = recv(clientSocket, recvbuf, 200, 0);
+        recvbuf[iDataNum] = '\0';
+        printf("recv data of my world is: %s\n", recvbuf);
+    }
+    close(clientSocket);
+    return 0;
+}
