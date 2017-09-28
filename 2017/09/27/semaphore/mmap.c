dc953bcb0983339c4d2c783012f1adc72755846c
zhangrxiang <599490911@qq.com>
2017-09-28 09:17:08 +0800

    mmap() ???

---
+++ 2017/09/27/semaphore/mmap.c
@@ -0,0 +1,41 @@
+//
+// Created by zhangrongxiang on 2017/9/27 14:21
+// File mmap
+//
+
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+
+typedef struct {
+    char name[4];
+    int age;
+} people;
+
+void main(int argc, char **argv) // map a normal file as shared mem:
+{
+    int fd, i;
+    people *p_map;
+    char temp;
+    fd = open(argv[1], O_CREAT | O_RDWR | O_TRUNC, 00777);
+    lseek(fd, sizeof(people) * 5 - 1, SEEK_SET);
+    write(fd, "", 1);
+    p_map = (people *) mmap(NULL, sizeof(people) * 10, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    printf("-----------------------\n");
+    close(fd);
+    temp = 'a';
+    for (i = 0; i < 10; i++) {
+        printf("-----------------------\n");
+        temp += 1;
+        memcpy((*(p_map + i)).name, &temp, 2);
+        (*(p_map + i)).age = 20 + i;
+    }
+    printf(" initialize over \n ");
+    sleep(10);
+    munmap(p_map, sizeof(people) * 10);
+    printf("umap ok \n");
+}
