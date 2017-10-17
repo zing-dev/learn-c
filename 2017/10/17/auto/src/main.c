9052a514b24aa12a1ce8d9695fcedea0f99b8cef
zhangrxiang <599490911@qq.com>
2017-10-17 15:55:48 +0800

    autotools  gnu soft

---
+++ 2017/10/17/auto/src/main.c
@@ -0,0 +1,11 @@
+//
+// Created by zhangrongxiang on 2017/10/17 14:57
+// File main
+//
+#include <../config.h>
+#include <stdio.h>
+
+int main() {
+    puts("Hello World!");
+    printf("%s\n", PACKAGE_BUGREPORT);
+}
---
+++ 2017/10/17/auto/src/p2/p2.c
@@ -0,0 +1,11 @@
+//
+// Created by zhangrongxiang on 2017/10/17 15:10
+// File p1
+//
+
+
+#include <stdio.h>
+
+int main() {
+    puts("this is p2 ");
+}
