6ae5d88da1dcac0b16d744f036f7cf131be37848
zhangrxiang <599490911@qq.com>
2017-10-18 17:07:28 +0800

    sqlite3 c api

---
+++ 2017/10/18/sqlite/main.c
@@ -0,0 +1,24 @@
+//
+// Created by zhangrongxiang on 2017/10/18 16:33
+// File main
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+
+int main(int argc, char *argv[]) {
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+
+    rc = sqlite3_open("test.db", &db);
+
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        exit(0);
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+    sqlite3_close(db);
+}
