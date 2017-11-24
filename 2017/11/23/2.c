1d9f8fd6046f4f12e496a0c969e2136750942260
zhangrxiang <599490911@qq.com>
2017-11-24 09:25:22 +0800

    int[] -> char[] -> int[]

--- 2017/11/23/2.c
+++ 2017/11/23/2.c
@@ -4,6 +4,7 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     unsigned int ids[100] = {0}, *pids;
@@ -11,7 +12,7 @@ int main() {
     unsigned int *tmp;
     int i = 0, j = 0;
     for (; i < 100; ++i) {
-        ids[i] = i;
+        ids[i] = i * i;
     }
     pcids = cids;
     pids = ids;
@@ -21,7 +22,6 @@ int main() {
         pcids[i] = (pids + i * sizeof(char));
         printf("%8x  ----  %8x\n",(pids + i * sizeof(char)),pcids[i]);
     }
-     */
     for (i = 0; i < 400; i += 4) {
         char s[4];
         for (j = 0; j < 4; j++) {
@@ -32,10 +32,16 @@ int main() {
         cids[i + 2] = s[2];
         cids[i + 3] = s[3];
     }
+     */
+    memcpy(cids, ids, sizeof(ids));
     printf("----------------------\n");
-    for (i = 0; i < 100; ++i) {
-        printf("%d\n", (int) *(pcids + i * sizeof(int)));
-    }
 
+    for (i = 0; i < 100; ++i) {
+//        printf("%d\n", (int) *(pcids + i * sizeof(int)));
+        tmp = (unsigned int *) (pcids + sizeof(unsigned int) * i);
+        printf("%d\n", *tmp);
+    }
+    printf("unsigned int   --> %d\n", sizeof(unsigned int));
+    printf("unsigned char  --> %d\n", sizeof(unsigned char));
 
 }
