9671d1ae30dbbb60f51b33ccfaa01c2536f55f63
zhangrxiang <599490911@qq.com>
2017-11-29 16:50:17 +0800

    int -> char[] -> int

--- 2017/11/29/1.c
+++ 2017/11/29/1.c
@@ -7,13 +7,28 @@
 #include <string.h>
 
 int main() {
-    int ii[] = {2, 3, [10] = 10};
-    unsigned char index[4];
-    //memcpy(index, ii, sizeof(int) * 2);
+    unsigned int ii[] = {5, 3, [5] = 10}, iii = 111;
+    unsigned char index[sizeof(ii)];
+    unsigned char index2[4];
+
     memcpy(index, ii, sizeof(ii));
+    memcpy(index2, &iii, sizeof(iii));
     unsigned int *pInt;
-    for (int i = 0; i < 11; ++i) {
+
+    for (int i = 0; i < (sizeof(ii) / sizeof(unsigned int)); ++i) {
         pInt = (unsigned int *) (index + sizeof(unsigned int) * i);
         printf("%d\n", *pInt);
     }
+
+    printf("-----------------------------------------------------\n");
+    printf("sizeof(ii) --> %ld\n", sizeof(ii));
+    printf("sizeof(ii) --> %ld\n", sizeof(unsigned int) * 11);
+    printf("-----------------------------------------------------\n");
+    printf("%d\n", *(unsigned int *) (index));
+    printf("-----------------------------------------------------\n");
+    //memset(index2, iii, sizeof(unsigned int));
+    printf("%d\n", *(unsigned int *) (index2));
+    printf("-----------------------------------------------------\n");
+    printf("sizeof(int)  -->  %ld\n", sizeof(int));
+    printf("sizeof(unsigned int)  -->  %ld\n", sizeof(unsigned int));
 }
