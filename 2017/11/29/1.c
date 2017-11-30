e56bc09f11970314a3d31ef0ff8419730e4fd73d
zhangrxiang <599490911@qq.com>
2017-11-30 18:03:03 +0800

    ..

--- 2017/11/29/1.c
+++ 2017/11/29/1.c
@@ -6,6 +6,11 @@
 #include <stdio.h>
 #include <string.h>
 
+void get(void *data) {
+    int *pInt;
+    pInt = (int *) (data);
+    printf(">>>>>>>>>>>>>> %d\n", *pInt);
+}
 int main() {
     unsigned int ii[] = {5, 3, [5] = 10}, iii = 111;
     unsigned char index[sizeof(ii)];
@@ -13,6 +18,8 @@ int main() {
 
     memcpy(index, ii, sizeof(ii));
     memcpy(index2, &iii, sizeof(iii));
+//    memcpy(index3, &iii, sizeof(iii));
+    get(index2);
     unsigned int *pInt;
 
     for (int i = 0; i < (sizeof(ii) / sizeof(unsigned int)); ++i) {
@@ -24,10 +31,11 @@ int main() {
     printf("sizeof(ii) --> %ld\n", sizeof(ii));
     printf("sizeof(ii) --> %ld\n", sizeof(unsigned int) * 11);
     printf("-----------------------------------------------------\n");
-    printf("%d\n", *(unsigned int *) (index));
+    printf("*(unsigned int *) (index)  --> %d\n", *(unsigned int *) (index));
     printf("-----------------------------------------------------\n");
     //memset(index2, iii, sizeof(unsigned int));
-    printf("%d\n", *(unsigned int *) (index2));
+    printf("*(unsigned int *) (index2) --> %d\n", *(unsigned int *) (index2));
+//    printf("*(unsigned int *) (index3) --> %d\n", *(unsigned int *) (index3));
     printf("-----------------------------------------------------\n");
     printf("sizeof(int)  -->  %ld\n", sizeof(int));
     printf("sizeof(unsigned int)  -->  %ld\n", sizeof(unsigned int));
