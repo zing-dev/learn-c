268b29ee780b4cc9b4f736d8a2ebd35b31a06047
zhangrxiang <599490911@qq.com>
2017-12-11 16:00:35 +0800

    enum

---
+++ 2017/12/11/enum.c
@@ -0,0 +1,32 @@
+//
+// Created by zhangrongxiang on 2017/12/11 14:03
+// File enum
+//
+
+
+#include <stdio.h>
+
+#ifdef TRUE
+#undef TRUE
+#endif
+
+#ifdef FALSE
+#undef FALSE
+#endif
+
+typedef enum {
+    FALSE,
+    TRUE,
+    TWO
+} Boolean;
+
+int main() {
+
+    Boolean t = TRUE;
+    Boolean f = FALSE;
+    printf("%d\n",FALSE);
+    printf("%d\n",TRUE);
+    printf("%d\n",TWO);
+    printf("%d\n",t);
+    printf("%d\n",f);
+}
