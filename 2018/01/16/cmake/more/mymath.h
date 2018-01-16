5e5572b5edc67f801886aca2c2e27ecd4258b66a
zhangrxiang <599490911@qq.com>
2018-01-16 15:27:16 +0800

    cmake使用

---
+++ 2018/01/16/cmake/more/mymath.h
@@ -0,0 +1,11 @@
+//
+// Created by zhangrongxiang on 2018/1/16 14:36
+// File mymath
+//
+
+#ifndef LEARC_C_MYMATH_H
+#define LEARC_C_MYMATH_H
+
+extern double power(double base, int exponent);
+
+#endif //LEARC_C_MYMATH_H
