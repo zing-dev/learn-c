108528d78c0d8b47284740ff2b5c18ea4889fcff
zhangrxiang <599490911@qq.com>
2018-02-08 18:09:12 +0800

    memchr() memcmp()

---
+++ string/memchr.c
@@ -0,0 +1,34 @@
+//
+// Created by zhangrongxiang on 2018/2/8 14:10
+// File memchr
+//
+
+//C 库函数 void *memchr(const void *str, int c, size_t n)
+// 在参数 str 所指向的字符串的前 n 个字节中搜索第一次出现字符 c（一个无符号字符）的位置。
+
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+    //字符串
+    char str[] = "I am your God";
+    //数组
+    int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+    char *position = (char *) memchr(str, 'y', strlen(str));
+    if (position) {
+        printf("%s\n", position);//your God
+        printf("%d\n", (int) (position - str));//5
+        printf("%c\n", str[position - str]); //y
+    } else {
+        printf("null\n");
+    }
+    int *pInt = (int *) memchr(arr, 50, sizeof(arr));
+    printf("%d\n", *pInt);//50
+    printf("%c\n", *pInt);//2
+    printf("%c\n", (unsigned char) 50); //2
+    printf("%c\n", (char) 50); //2
+    printf("%c\n", 50);//2
+    printf("%c\n", (unsigned char) 51); // 3
+
+    return 0;
+}
---
+++ string/memcmp.c
@@ -0,0 +1,53 @@
+//
+// Created by zhangrongxiang on 2018/2/8 16:57
+// File memcmp
+//
+
+#include <stdio.h>
+#include <string.h>
+
+//C 库函数 int memcmp(const void *str1, const void *str2, size_t n)) 把存储区 str1 和存储区 str2 的前 n 个字节进行比较。
+//比较按字典序进行。
+typedef struct {
+    char *name;
+} Stu;
+
+int main() {
+    char *str = "abc";
+    char *str2 = "abC";
+    int cmp = memcmp(str, str2, strlen(str) > strlen(str2) ? strlen(str) : strlen(str2));
+    printf("%d\n", cmp); // windows mingw -> 1  linux gcc 32
+
+    int i = 0;
+    int i2 = 10;
+    cmp = memcmp(&i, &i2, sizeof(int));
+    printf("%d\n", cmp);//windows mingw -> -1  linux gcc -10
+    printf("%d\n", (int) sizeof(short));//2
+
+    Stu stu = {.name = "zing"};
+    Stu stu2 = {.name = "zing"};
+    Stu stu3 = {.name = "Zing"};
+    Stu stu4 = {.name = "aing"};
+
+    /***********************************************/
+    printf("%d\n", (int) sizeof(Stu)); //8
+    printf("%d\n", (int) sizeof(stu.name)); //8
+    printf("%d\n", (int) strlen(stu.name)); //4
+    printf("%d\n", (int) sizeof(char *)); //8
+    printf("%d\n", (int) sizeof(int *)); //8
+    printf("%d\n", (int) sizeof(long *)); //8
+    /***********************************************/
+
+    cmp = memcmp(&stu, &stu2, sizeof(Stu));
+    printf("struct %d \n", cmp);//0
+    cmp = memcmp(stu.name, stu3.name, sizeof(Stu));
+    printf("struct %d \n", cmp); // windows mingw -> -1  linux gcc 32
+    cmp = memcmp(stu.name, stu4.name, sizeof(Stu));
+    printf("struct %d \n", cmp); // windows mingw -> -1  linux gcc 25
+
+    cmp = memcmp(&"abC", &"abc", sizeof(char *));
+    printf("%d\n", cmp);//-32
+    printf("a -> %d;A -> %d \n", 'a', 'A');//a -> 97;A -> 65
+    printf("z -> %d;a -> %d \n", 'z', 'a');//a -> 97;A -> 65
+    return 0;
+}
