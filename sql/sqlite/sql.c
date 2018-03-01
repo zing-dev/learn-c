70adccbee46957260b6f6cb7995cece01a44424e
zhangrxiang <599490911@qq.com>
2018-03-01 18:08:59 +0800

    sqlite sqlite3_exec() 回调函数使用

--- sql/sqlite/sql.c
+++ sql/sqlite/sql.c
@@ -6,7 +6,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "sqlite3.h"
+#include <sqlite3.h>
+
+int queryCallback(void *stu, int argc, char **argv, char **azColName);
 
 typedef struct STUDENT {
     unsigned int id;
@@ -65,14 +67,48 @@ int insertTable(sqlite3 *db, Student student) {
     return rc;
 }
 
-int queryTable(sqlite3 *db, Student *student, int count) {
-    char *sql = "select * from student limit 10";
+/*
+ * @param stu
+ * @param argc
+ * @param argv
+ * @param azColName
+ * @return
+ */
+int queryCallback(void *stu, int argc, char **argv, char **azColName) {
+    static int j;
+    Student *student = stu;
+    student[j].id = (unsigned int) atoi(argv[0]);
+    memcpy(student[j].name, argv[1], sizeof(student[j].name));
+    student[j].age = (unsigned int) atoi(argv[2]);
+    memcpy(student[j].address, argv[3], sizeof(student[j].address));
+    j++;
+//    printf("\n");
+//    for (i = 0; i < argc; i++) {
+//        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+//    }
+//    printf("argc-->%d\n",argc);
+    return 0;
+}
+
+int query(sqlite3 *db, Student *student, int index, int count) {
+    char sql[128];
+    sprintf(sql, "select * from student order by id desc limit %d,%d;", index, count);
     char **err = NULL;
+    return sqlite3_exec(db, sql, queryCallback, student, err);
+}
+
+int queryTable(sqlite3 *db, Student *student, int index, int count) {
+    char sql[128];
+    if (count > 10)
+        count = 10;
+    sprintf(sql, "select * from student order by id desc limit %d,%d", index, count);
+    printf("%s\n", sql);
+    char *err = NULL;
     int col, row, i, j = 0;
     char **result;
-    int rs = sqlite3_get_table(db, sql, &result, &row, &col, err);
+    int rs = sqlite3_get_table(db, sql, &result, &row, &col, &err);
     if (rs == SQLITE_OK) {
-        printf("row %d,col %d\n", row, col);
+        printf("row %d, col %d\n", row, col);
 //        id-name-age-address
 //        printf("%s-%s-%s-%s\n", result[0], result[1], result[2], result[3]);
 //        1-zing0-10-jiangsu-wuxi-0
@@ -87,17 +123,67 @@ int queryTable(sqlite3 *db, Student *student, int count) {
                 memcpy(student[j].name, result[i + 1], sizeof(student[i].name));
                 student[j].age = (unsigned int) atoi(result[i + 2]);
                 memcpy(student[j].address, result[i + 3], sizeof(student[i].address));
-                /*printf("id:%d,name:%s,age:%d,address:%s\n", student[j].id, student[j].name, student[j].age,
-                       student[j].address);
-                printf("id:%s,name:%s,age:%s,address:%s\n", result[i], result[i + 1], result[i + 2],
-                       result[i + 3]);*/
+//                printf("id:%d,name:%s,age:%d,address:%s\n", student[j].id, student[j].name, student[j].age, student[j].address);
+//                printf("id:%s,name:%s,age:%s,address:%s\n", result[i], result[i + 1], result[i + 2], result[i + 3]);
                 j++;
             }
         }
     } else {
         printf("error\n");
     }
+    return rs;
 }
+
+int update(sqlite3 *db, Student student) {
+
+    char sql[128];
+    sprintf(sql, "UPDATE student set address = '%s' where ID=%d;", student.address, student.id);
+    char *err = NULL;
+    /* Execute SQL statement */
+    int rc = sqlite3_exec(db, sql, NULL, (void *) 0, &err);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", err);
+        sqlite3_free(err);
+    } else {
+        fprintf(stdout, "Operation done successfully\n");
+    }
+    return rc;
+}
+
+int update2(sqlite3 *db, int id, char *address) {
+
+    char sql[128];
+    sprintf(sql, "UPDATE student set address = '%s' where ID=%d;", address, id);
+    char *err = NULL;
+    /* Execute SQL statement */
+    int rc = sqlite3_exec(db, sql, NULL, (void *) 0, &err);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", err);
+        sqlite3_free(err);
+    } else {
+        fprintf(stdout, "Operation done successfully\n");
+    }
+    return rc;
+}
+
+int update3(sqlite3 *db, void *data) {
+    Student *student = (Student *) data;
+    printf("-->  %d\n", student->id);
+    printf("-->  %s\n", student->address);
+    char sql[128];
+    sprintf(sql, "UPDATE student set address = '%s' where ID=%d;", student->address, student->id);
+    char *err = NULL;
+    /* Execute SQL statement */
+    int rc = sqlite3_exec(db, sql, NULL, (void *) 0, &err);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", err);
+        sqlite3_free(err);
+    } else {
+        fprintf(stdout, "Operation done successfully\n");
+    }
+    return rc;
+}
+
 
 int main() {
     int i = 0;
@@ -110,17 +196,55 @@ int main() {
     if (r == 0) {
         printf("open sb success\n");
     }
-    generateStudent(student, count);
-    printf("----------==ok---------\n");
-    for (i = 0; i < count; ++i) {
-        printf("id:%d,name:%s,age:%d,address:%s\n", student[i].id, student[i].name, student[i].age, student[i].address);
-    }
-    createTable(db);
+//    generateStudent(student, count);
+//    printf("----------==ok---------\n");
+//    for (i = 0; i < count; ++i) {
+//        printf("id:%d,name:%s,age:%d,address:%s\n", student[i].id, student[i].name, student[i].age, student[i].address);
+//    }
+//    createTable(db);
 //    for (i = 0; i < count; ++i) {
 //        insertTable(db, student[i]);
 //    }
-    queryTable(db, student, 10);
+    if (queryTable(db, student, 2, 10) == SQLITE_OK) {
+        for (i = 0; i < count; ++i) {
+            printf("id:%d,name:%s,age:%d,address:%s\n", student[i].id, student[i].name, student[i].age,
+                   student[i].address);
+        }
+    }
 
+    Student stu;
+    stu.id = 68;
+    char *address = "jiangsu-xuzhou";
+    memcpy(stu.address, address, strlen(address));
+    update(db, stu);
+    queryTable(db, student, 0, 10);
+    for (i = 0; i < count; ++i) {
+        printf("id:%d,name:%s,age:%d,address:%s\n", student[i].id, student[i].name, student[i].age,
+               student[i].address);
+    }
+    update2(db, 69, address);
+    queryTable(db, student, 0, 10);
+    for (i = 0; i < count; ++i) {
+        printf("id:%d,name:%s,age:%d,address:%s\n", student[i].id, student[i].name, student[i].age,
+               student[i].address);
+    }
+    address = "jiangsu-suzhou";
+    stu.id = 70;
+    memcpy(stu.address, address, strlen(address));
+    update3(db, &stu);
+    if (queryTable(db, student, 0, 10)) {
+        for (i = 0; i < count; ++i) {
+            printf("update3-> id:%d,name:%s,age:%d,address:%s\n", student[i].id, student[i].name, student[i].age,
+                   student[i].address);
+        }
+    }
+    memset(student, 0, sizeof(student));
+    if (query(db, student, 0, 10) == SQLITE_OK) {
+        for (i = 0; i < count; ++i) {
+            printf("update-> id:%d,name:%s,age:%d,address:%s\n", student[i].id, student[i].name, student[i].age,
+                   student[i].address);
+        }
+    }
     sqlite3_close(db);
     return 0;
 }
